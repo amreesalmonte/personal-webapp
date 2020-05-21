bb8ea7162d87176958f46029bc8df2ac6d060407
amreesalmonte <amreesalmonte@gmail.com>
2020-05-21 01:42:30 -0600

    added for components

---
+++ src/data/experience.jsx
@@ -0,0 +1,18 @@
+const experiencesData = [
+    {
+        header: "Teaching Assistant",
+        subHeader: "University of Alberta (Jan 2020 - April 2020)",
+        descriptions: "Supervised a lab of 20 students that supplements an introductory computing science class and aided students with programming assignments by teaching them good coding practices and methods to debug code",
+        githubLink: null,
+        websiteLink: null
+    },
+    {
+        header: "Volunteer Tutor",
+        subHeader: "Ada's Team (Sep 2019 - Present)",
+        descriptions: "Tutored over 50 students in a variety of computing science topics such as Python, SQL queries, and Object Oriented Programming and lead discussions with a group of students to solve assignments and labs through whiteboard examples",
+        githubLink: null,
+        websiteLink: null
+    }
+]
+
+export default experiencesData;
